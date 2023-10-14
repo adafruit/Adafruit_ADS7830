3821a9ce04fa1ee082b96511193bd48812f8e65b
ladyada <limor@ladyada.net>
2023-10-14 14:38:39 -0400

    init

---
+++ Adafruit_AD7830.cpp
@@ -0,0 +1,123 @@
+/*!
+ * @file Adafruit_AD7830.cpp
+ *
+ * @mainpage Adafruit AD7830 ADC library
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's AD7830 driver for the
+ * Arduino platform. It is designed specifically to work with the
+ * Adafruit AD7830 breakout: https://www.adafruit.com/product/xxxx
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the board.
+ *
+ * Adafruit invests time and resources providing this open-source code.
+ * Please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section dependencies Dependencies
+ * - Adafruit_BusIO
+ *
+ * @section author Author
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ * MIT license, all text here must be included in any redistribution.
+ *
+ */
+
+#include "Adafruit_AD7830.h"
+
+
+/**
+ * @brief Construct a new Adafruit_AD7830 object.
+ * 
+ * Initializes the i2c_dev member variable to nullptr.
+ */
+Adafruit_AD7830::Adafruit_AD7830() {
+  i2c_dev = nullptr;
+}
+
+/**
+ * @brief Initialize the AD7830 chip.
+ * 
+ * @param i2c_addr I2C address of the AD7830 chip.
+ * @param theWire Pointer to the TwoWire object.
+ * @return true if initialization is successful, false otherwise.
+ */
+bool Adafruit_AD7830::begin(uint8_t i2c_addr, TwoWire *theWire) {
+  if (i2c_dev) {
+    delete i2c_dev;
+  }
+
+  i2c_dev = new Adafruit_I2CDevice(i2c_addr, theWire);
+
+  return i2c_dev->begin();
+}
+
+
+/**
+ * @brief Read ADC value in single-ended mode.
+ * 
+ * @param ch Channel to read from (0-7).
+ * @param pd Power-down selection mode.
+ * @return ADC value or 0 if read failed.
+ */
+uint8_t Adafruit_AD7830::readADCsingle(uint8_t ch, ad7830PowerDownSelection pd) {
+  if (ch > 7) {
+    return 0; // Invalid channel
+  }
+
+  uint8_t commandByte = 0;
+  
+  if (ch % 2 == 0) {
+    commandByte |= (SINGLE_CH0 + (ch / 2));
+    Serial.println(commandByte, HEX);
+  } else {
+    commandByte |= (SINGLE_CH1 + ((ch - 1) / 2));
+    Serial.println(commandByte, HEX);
+  }
+  commandByte <<= 4;
+
+  commandByte |= (pd << 2); // Set power-down bits
+
+  uint8_t adcValue;
+  if (!i2c_dev->write_then_read(&commandByte, 1, &adcValue, 1)) {
+    return 0; // Write-then-read failed
+  }
+
+  return adcValue;
+}
+
+
+/**
+ * @brief Read ADC value in differential mode.
+ * 
+ * @param ch Positive channel for differential reading (0-7).
+ * @param pd Power-down selection mode.
+ * @return ADC value or 0 if read failed.
+ * 
+ * The negative channel for differential reading is determined by the chip and is fixed.
+ * For example, if ch is 0, the negative channel will be 1.
+ */
+uint8_t Adafruit_AD7830::readADCdifferential(uint8_t ch, ad7830PowerDownSelection pd) {
+  if (ch > 7) {
+    return 0; // Invalid channel
+  }
+
+  uint8_t commandByte = 0;
+
+  // Set channel bits for differential mode
+  commandByte |= (DIFF_CH0_CH1 + (ch * 2));
+  commandByte <<= 4;
+
+  commandByte |= (pd << 2); // Set power-down bits
+
+  uint8_t adcValue;
+  if (!i2c_dev->write_then_read(&commandByte, 1, &adcValue, 1)) {
+    return 0; // Write-then-read failed
+  }
+
+  return adcValue;
+}
---
+++ Adafruit_AD7830.h
@@ -0,0 +1,48 @@
+#ifndef ADAFRUIT_AD7830_H
+#define ADAFRUIT_AD7830_H
+
+#include <Adafruit_I2CDevice.h>
+
+// Default I2C Address
+#define AD7830_I2C_ADDR 0x48 ///< Default I2C Address
+
+// Power-Down Selection Enum
+typedef enum {
+  POWER_DOWN_BETWEEN_CONVERSIONS = 0x00, ///< Power Down Between A/D Converter Conversions
+  INTERNAL_REF_OFF_ADC_ON = 0x01, ///< Internal Reference OFF and A/D Converter ON
+  INTERNAL_REF_ON_ADC_OFF = 0x02, ///< Internal Reference ON and A/D Converter OFF
+  INTERNAL_REF_ON_ADC_ON = 0x03  ///< Internal Reference ON and A/D Converter ON
+} ad7830PowerDownSelection;
+
+// Channel Selection Control Enum
+typedef enum {
+  DIFF_CH0_CH1 = 0x00, ///< Differential CH0-CH1
+  DIFF_CH2_CH3 = 0x01, ///< Differential CH2-CH3
+  DIFF_CH4_CH5 = 0x02, ///< Differential CH4-CH5
+  DIFF_CH6_CH7 = 0x03, ///< Differential CH6-CH7
+  DIFF_CH1_CH0 = 0x04, ///< Differential CH1-CH0
+  DIFF_CH3_CH2 = 0x05, ///< Differential CH3-CH2
+  DIFF_CH5_CH4 = 0x06, ///< Differential CH5-CH4
+  DIFF_CH7_CH6 = 0x07, ///< Differential CH7-CH6
+  SINGLE_CH0 = 0x08, ///< Single-Ended CH0
+  SINGLE_CH1 = 0x0C, ///< Single-Ended CH1
+  SINGLE_CH2 = 0x09, ///< Single-Ended CH2
+  SINGLE_CH3 = 0x0D, ///< Single-Ended CH3
+  SINGLE_CH4 = 0x0A, ///< Single-Ended CH4
+  SINGLE_CH5 = 0x0E, ///< Single-Ended CH5
+  SINGLE_CH6 = 0x0B, ///< Single-Ended CH6
+  SINGLE_CH7 = 0x0F  ///< Single-Ended CH7
+} ad7830ChannelSelectionControl;
+
+class Adafruit_AD7830 {
+public:
+  Adafruit_AD7830();
+  bool begin(uint8_t i2c_addr = AD7830_I2C_ADDR, TwoWire *theWire = &Wire);
+  uint8_t readADCsingle(uint8_t ch, ad7830PowerDownSelection pd = INTERNAL_REF_ON_ADC_ON);
+  uint8_t readADCdifferential(uint8_t ch, ad7830PowerDownSelection pd = INTERNAL_REF_ON_ADC_ON);
+
+private:
+  Adafruit_I2CDevice *i2c_dev; // I2C device
+};
+
+#endif // ADAFRUIT_AD7830_H
